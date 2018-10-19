1deb1226e52559dfa578270393c4015034c81b6a
Desetude <harry@desetude.com>
2018-10-19 16:59:49 +0100

    Instruction additions (and start of cycle work)

--- src/main/java/com/harryfreeborough/nesemu/instruction/InstructionProcessor.java
+++ src/main/java/com/harryfreeborough/nesemu/instruction/InstructionProcessor.java
@@ -1,9 +1,34 @@
 package com.harryfreeborough.nesemu.instruction;
 
 import com.harryfreeborough.nesemu.Cpu;
+import com.harryfreeborough.nesemu.CpuState;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public interface InstructionProcessor {
 
     void execute(Cpu cpu, AddressingMode mode);
+
+    static InstructionProcessor load(BiConsumer<CpuState, Integer> consumer) {
+        return (cpu, mode) -> {
+            CpuState state = cpu.getState();
+            int value = mode.read1(cpu);
+            state.flagZ = value == 0;
+            state.flagN = (value & 0b10000000) != 0;
+            consumer.accept(state, value);
+        };
+    }
+
+    static InstructionProcessor compare(Function<CpuState, Integer> function) {
+        return (cpu, mode) -> {
+            CpuState state = cpu.getState();
+            int value = mode.read1(cpu);
+            int register = function.apply(state);
+            state.flagZ = register == value;
+            state.flagC = register >= value;
+            state.flagN = (value & 0b10000000) != 0;
+        };
+    }
 
 }
